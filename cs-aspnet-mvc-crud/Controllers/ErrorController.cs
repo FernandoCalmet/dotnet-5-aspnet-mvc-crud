25ac311fc0043e346cc8875db4e3dde03533e318
Fernando Calmet <fercalmet@gmail.com>
2021-10-26 12:09:19 -0500

    more changes

---
+++ cs-aspnet-mvc-crud/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace cs_aspnet_mvc_crud.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
